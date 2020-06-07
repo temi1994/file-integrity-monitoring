e21399fd33ab06a7d398269c952527f344d636b0
temi1994 <43004739+temi1994@users.noreply.github.com>
2020-06-07 13:48:16 +0430

    Add files via upload

---
+++ fim.py
@@ -0,0 +1,53 @@
+import hashlib
+import time
+import datetime
+import ctypes
+
+id = 1
+print("Enter your file name with absolute directory:")
+file_1 = input()
+hasher = hashlib.md5()
+with open(file_1, 'rb') as afile:
+    buf = afile.read()
+    hasher.update(buf)
+    test_1 = hasher.hexdigest()
+    content_1 = buf
+#print(test_1)
+
+while id > 0:
+
+    time.sleep(10)
+
+    #print("Sleep Working!")
+
+    hasher = hashlib.md5()
+    with open(file_1, 'rb') as afile:
+        buf = afile.read()
+        hasher.update(buf)
+        test_2 = hasher.hexdigest()
+        now = datetime.datetime.now()
+        content_2 = buf
+    #print(test_2)
+
+
+    if test_1 != test_2 :
+        test_1 = test_2
+        ctypes.windll.user32.MessageBoxW(0, "Warning!", "File Integrity Monitoring Alert", 0)
+        print("Alert!")
+        with open("log.txt", "w") as text_file:
+            text_file.write(now.strftime("%Y-%m-%d %H:%M:%S"))
+            text_file.write("  ")
+            text_file.write("Warning! File ")
+            text_file.write(file_1)
+            text_file.write(" is Modified!")
+            text_file.write("\n")
+            text_file.write("\t")
+            text_file.write("Before Modified >> ")
+            text_file.write(str(content_1))
+            text_file.write("\n")
+            text_file.write("\t")
+            text_file.write("After Modified >>  ")
+            text_file.write(str(content_2))
+        
+
+
